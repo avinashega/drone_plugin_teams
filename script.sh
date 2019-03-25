fa704b1dcb0547817aa66620eaa0876537219582
Avinash Ega <avinash.ega@gmail.com>
2019-03-25 16:31:43 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [ -z "$PLUGIN_WEBHOOK" ]; then
-	if [ -z "$TEAMS_WEBHOOK" ]; then
-		echo "Need to set teams_webhook URL"
+	if [ -z "$TEAMS_WEBHOOK_URL" ]; then
+		echo "Need to set teams_webhook_url value"
 		exit 1
 	else
-		WEBHOOK="$TEAMS_WEBHOOK"
+		WEBHOOK="$TEAMS_WEBHOOK_URL"
 	fi
 else
 	WEBHOOK="$PLUGIN_WEBHOOK"
